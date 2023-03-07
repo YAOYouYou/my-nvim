2da4eb17dd7e3b589b019ebfb926766b813ab1e0
youyou <705592307@qq.com>
2023-03-07 23:09:40 +0800

    disable commit confirm

--- lua/vcs/neogit.lua
+++ lua/vcs/neogit.lua
@@ -1,6 +1,7 @@
 local neogit = require("neogit")
 local config = {
-		integrations = { diffview = true },
+	integrations = { diffview = true },
+	disable_commit_confirmation = false,
 }
 
 neogit.setup(config)
