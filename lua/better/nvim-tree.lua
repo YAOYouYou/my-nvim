8a37ee35173be63b3210265352c0e3710981c10a
youyou <705592307@qq.com>
2023-03-14 21:42:29 +0800

    这些配置basic已有

--- lua/better/nvim-tree.lua
+++ lua/better/nvim-tree.lua
@@ -1,11 +1,3 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
-
 local config = {
 -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
