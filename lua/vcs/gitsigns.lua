a35b0a01920796f275cc83a772a48048814a8453
youyou <705592307@qq.com>
2023-03-18 15:40:12 +0800

    update gitsigns current_blame delay time.

--- lua/vcs/gitsigns.lua
+++ lua/vcs/gitsigns.lua
@@ -26,6 +26,12 @@ local config = {
       linehl = "GitSignsChangeLn",
     },
   },
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 10,
+    ignore_whitespace = false,
+  },
   -- update_debounce = 100,
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
