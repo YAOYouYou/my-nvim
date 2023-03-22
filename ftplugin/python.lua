edbd3ce675d30b7afe53e32d0ae3ff84af751dc4
youyou <705592307@qq.com>
2023-03-22 22:02:33 +0800

    fix ftplugin python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,21 +1,21 @@
 -- 设置 tab 宽度为 4
-vim.op_local.expandtab = true
-vim.op_local.tabstop = 4
-vim.op_local.softtabstop = 4
-vim.op_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
 
 -- 启用自动缩进
-vim.op_local.autoindent = true
-vim.op_local.smartindent = true
+vim.opt_local.autoindent = true
+vim.opt_local.smartindent = true
 
 -- 启用语法检查
-vim.op_local.spell = true
-vim.op_local.spelllang = "en"
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en"
 
 -- 启用代码折叠
-vim.op_local.foldmethod = "expr"
-vim.op_local.foldexpr = "nvim_treesitter#foldexpr()"
-vim.op_local.foldlevel = 99
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt_local.foldlevel = 99
 
 
 -- 设置蛇形命名风格
