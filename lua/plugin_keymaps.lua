20994cbcde7ddb29e7e1f0cc5bfd0d5d9c8f0ea0
youyou <705592307@qq.com>
2023-03-24 23:50:18 +0800

    add trouble keymap

--- lua/plugin_keymaps.lua
+++ lua/plugin_keymaps.lua
@@ -37,18 +37,18 @@ vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle, { desc = "UndotreeToggl
 -- lsp
 M.lsp_keymaps = function(bufnr)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "goto definition" })
-	vim.keymap.set('n', 'gD', "<cmd>Lspsaga peek_definition<cr>", {buffer=bufnr, desc="Peek Definition"})
+	vim.keymap.set('n', 'gD', "<cmd>Lspsaga peek_definition<cr>", { buffer = bufnr, desc = "Peek Definition" })
 	vim.keymap.set('n', 'gb', vim.lsp.buf.type_definition, { buffer = bufnr, desc = "goto type_definition" })
-	vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, {buffer=bufnr, desc = "goto implementation"})
-	vim.keymap.set("n", "gh",  vim.lsp.buf.hover, {buffer=bufnr, desc="hover"})
-	vim.keymap.set('n', 'gr', "<cmd>Lspsaga lsp_finder<cr>", {buffer=bufnr, desc = "references"})
-	vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, {buffer=bufnr, desc="format document"})
+	vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = "goto implementation" })
+	vim.keymap.set("n", "gh", vim.lsp.buf.hover, { buffer = bufnr, desc = "hover" })
+	vim.keymap.set('n', 'gr', "<cmd>Lspsaga lsp_finder<cr>", { buffer = bufnr, desc = "references" })
+	vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { buffer = bufnr, desc = "format document" })
 	-- TODO： 所选内容格式
 	-- vim.keymap.set('v', '<leader>lf', vim.lsp.buf.formatting, {buffer=bufnr, desc="format document"})
-	vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, {buffer=bufnr, desc="rename"})
-	vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, {buffer=bufnr, desc = "show diagnostic"})
-	vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, {buffer=bufnr, desc = "signature_help"})
-	vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, {buffer=bufnr, desc = "code action"})
+	vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { buffer = bufnr, desc = "rename" })
+	vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { buffer = bufnr, desc = "show diagnostic" })
+	vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, { buffer = bufnr, desc = "signature_help" })
+	vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { buffer = bufnr, desc = "code action" })
 end
 
 -- toggleterm
@@ -86,4 +86,11 @@ vim.keymap.set("n", "<Leader>dl", require 'dap'.run_last, opts)
 -- overseer
 vim.keymap.set("n", "<leader>ct", require("overseer").toggle, opts)
 vim.keymap.set('n', "<leader>cr", function() require('overseer').run_template({ name = "run script" }) end, opts)
+
+-- trouble
+vim.keymap.set('n', "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+	{ silent = true, desc = "toggle workspace_diagnostics" })
+vim.keymap.set('n', "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+	{ silent = true, desc = "toggle workspace_diagnostics" })
+
 return M
