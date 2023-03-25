4caaeeaa8159833a671edd8c94b5389648de791a
youyou <705592307@qq.com>
2023-03-25 21:16:45 +0800

    update test

--- lua/plugins/test/init.lua
+++ lua/plugins/test/init.lua
@@ -1,158 +1,93 @@
-return {
-	"nvim-neotest/neotest",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-neotest/neotest-python",
-		"nvim-neotest/neotest-go",
-	},
-	config = function()
-		local config = {
-			adapters = {
-				require("neotest-python")({
-					-- Extra arguments for nvim-dap configuration
-					-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-					dap = { justMyCode = false },
-					-- Command line arguments for runner
-					-- Can also be a function to return dynamic values
-					args = { "--log-level", "DEBUG" },
-					-- Runner to use. Will use pytest if available by default.
-					-- Can be a function to return dynamic value.
-					runner = "pytest",
-					-- Custom python path for the runner.
-					-- Can be a string or a list of strings.
-					-- Can also be a function to return dynamic value.
-					-- If not provided, the path will be inferred by checking for
-					-- virtual envs in the local directory and for Pipenev/Poetry configs
-					python = vim.fn.trim(vim.fn.system("which python")),
-					-- Returns if a given file path is a test file.
-					-- NB: This function is called a lot so don't perform any heavy tasks within it.
-					-- is_test_file = function(file_path)
-					--   ...
-					-- end,
-				}),
-				require("neotest-go"),
-			},
-			benchmark = {
-				enabled = true,
-			},
-			consumers = {},
-			default_strategy = "integrated",
-			diagnostic = {
-				enabled = true,
-				severity = 1,
-			},
-			discovery = {
-				concurrent = 0,
-				enabled = true,
-			},
-			floating = {
-				border = "rounded",
-				max_height = 0.6,
-				max_width = 0.6,
-				options = {},
-			},
-			highlights = {
-				adapter_name = "NeotestAdapterName",
-				border = "NeotestBorder",
-				dir = "NeotestDir",
-				expand_marker = "NeotestExpandMarker",
-				failed = "NeotestFailed",
-				file = "NeotestFile",
-				focused = "NeotestFocused",
-				indent = "NeotestIndent",
-				marked = "NeotestMarked",
-				namespace = "NeotestNamespace",
-				passed = "NeotestPassed",
-				running = "NeotestRunning",
-				select_win = "NeotestWinSelect",
-				skipped = "NeotestSkipped",
-				target = "NeotestTarget",
-				test = "NeotestTest",
-				unknown = "NeotestUnknown",
-			},
-			icons = {
-				child_indent = "│",
-				child_prefix = "├",
-				collapsed = "─",
-				expanded = "╮",
-				failed = "",
-				final_child_indent = " ",
-				final_child_prefix = "╰",
-				non_collapsible = "─",
-				passed = "",
-				running = "",
-				running_animated = { "/", "|", "\\", "-", "/", "|", "\\", "-" },
-				skipped = "",
-				unknown = "",
-			},
-			jump = {
-				enabled = true,
-			},
-			log_level = 3,
-			output = {
-				enabled = true,
-				open_on_run = "short",
-			},
-			output_panel = {
-				enabled = true,
-				open = "botright split | resize 15",
-			},
-			projects = {},
-			quickfix = {
-				enabled = true,
-				open = true,
-			},
-			run = {
-				enabled = true,
-			},
-			running = {
-				concurrent = true,
-			},
-			state = {
-				enabled = true,
-			},
-			status = {
-				enabled = true,
-				signs = true,
-				virtual_text = false,
-			},
-			strategies = {
-				integrated = {
-					height = 40,
-					width = 120,
-				},
-			},
-			summary = {
-				animated = true,
-				enabled = true,
-				expand_errors = true,
-				follow = true,
-				mappings = {
-					attach = "a",
-					clear_marked = "M",
-					clear_target = "T",
-					debug = "d",
-					debug_marked = "D",
-					expand = { "<CR>", "<2-LeftMouse>" },
-					expand_all = "e",
-					jumpto = "i",
-					mark = "m",
-					next_failed = "J",
-					output = "o",
-					prev_failed = "K",
-					run = "r",
-					run_marked = "R",
-					short = "O",
-					stop = "u",
-					target = "t",
-				},
-				open = "botright vsplit | vertical resize 50",
-			},
-		}
+local get_python_path = require("plugins.utils.python").get_python_path
 
-		require("neotest").setup(config)
-	end,
+return {
+    {
+        "vim-test/vim-test",
+        keys = {
+            { "<leader>tc", "<cmd>w|TestClass<cr>",   desc = "Class" },
+            { "<leader>tf", "<cmd>w|TestFile<cr>",    desc = "File" },
+            { "<leader>tl", "<cmd>w|TestLast<cr>",    desc = "Last" },
+            { "<leader>tn", "<cmd>w|TestNearest<cr>", desc = "Nearest" },
+            { "<leader>ts", "<cmd>w|TestSuite<cr>",   desc = "Suite" },
+            { "<leader>tv", "<cmd>w|TestVisit<cr>",   desc = "Visit" },
+        },
+        config = function()
+            vim.g["test#strategy"] = "neovim"
+            vim.g["test#neovim#term_position"] = "belowright"
+            vim.g["test#neovim#preserve_screen"] = 1
+            vim.g["test#python#runner"] = "pyunit" -- pytest
+        end,
+    },
+    {
+        "nvim-neotest/neotest",
+        keys = {
+            { "<leader>tNF", "<cmd>w|lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", desc = "Debug File" },
+            { "<leader>tNL", "<cmd>w|lua require('neotest').run.run_last({strategy = 'dap'})<cr>",                desc = "Debug Last" },
+            { "<leader>tNa", "<cmd>w|lua require('neotest').run.attach()<cr>",                                    desc = "Attach" },
+            { "<leader>tNf", "<cmd>w|lua require('neotest').run.run(vim.fn.expand('%'))<cr>",                     desc = "File" },
+            { "<leader>tNl", "<cmd>w|lua require('neotest').run.run_last()<cr>",                                  desc = "Last" },
+            { "<leader>tNn", "<cmd>w|lua require('neotest').run.run()<cr>",                                       desc = "Nearest" },
+            { "<leader>tNN", "<cmd>w|lua require('neotest').run.run({strategy = 'dap'})<cr>",                     desc = "Debug Nearest" },
+            { "<leader>tNo", "<cmd>w|lua require('neotest').output.open({ enter = true })<cr>",                   desc = "Output" },
+            { "<leader>tNs", "<cmd>w|lua require('neotest').run.stop()<cr>",                                      desc = "Stop" },
+            { "<leader>tNS", "<cmd>w|lua require('neotest').summary.toggle()<cr>",                                desc = "Summary" },
+        },
+        dependencies = {
+            "vim-test/vim-test",
+            "nvim-neotest/neotest-python",
+            "nvim-neotest/neotest-plenary",
+            "nvim-neotest/neotest-vim-test",
+            "nvim-neotest/neotest-python",
+            "nvim-neotest/neotest-go",
+            "rouge8/neotest-rust",
+        },
+        config = function()
+            local opts = {
+                adapters = {
+                    require "neotest-python" {
+                        dap = { justMyCode = false },
+                        python = get_python_path(),
+                        runner = "pytest",
+                    },
+                    require "neotest-plenary",
+                    require "neotest-vim-test" {
+                        ignore_file_types = { "python", "vim", "lua" },
+                    },
+                    require "neotest-rust",
+                },
+                -- overseer.nvim
+                consumers = {
+                    overseer = require "neotest.consumers.overseer",
+                },
+                overseer = {
+                    enabled = true,
+                    force_default = true,
+                },
+            }
+            require("neotest").setup(opts)
+        end,
+    },
+    {
+        "stevearc/overseer.nvim",
+        keys = {
+            { "<leader>toR", "<cmd>OverseerRunCmd<cr>",       desc = "Run Command" },
+            { "<leader>toa", "<cmd>OverseerTaskAction<cr>",   desc = "Task Action" },
+            { "<leader>tob", "<cmd>OverseerBuild<cr>",        desc = "Build" },
+            { "<leader>toc", "<cmd>OverseerClose<cr>",        desc = "Close" },
+            { "<leader>tod", "<cmd>OverseerDeleteBundle<cr>", desc = "Delete Bundle" },
+            { "<leader>tol", "<cmd>OverseerLoadBundle<cr>",   desc = "Load Bundle" },
+            { "<leader>too", "<cmd>OverseerOpen<cr>",         desc = "Open" },
+            { "<leader>toq", "<cmd>OverseerQuickAction<cr>",  desc = "Quick Action" },
+            { "<leader>tor", "<cmd>OverseerRun<cr>",          desc = "Run" },
+            { "<leader>tos", "<cmd>OverseerSaveBundle<cr>",   desc = "Save Bundle" },
+            { "<leader>tot", "<cmd>OverseerToggle<cr>",       desc = "Toggle" },
+        },
+        config = true,
+    },
+    -- {
+    --   "andythigpen/nvim-coverage",
+    --   cmd = { "Coverage" },
+    --   config = true,
+    -- },
 }
+
