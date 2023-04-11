98f7e68db8678072ddf8b501d453719cc2656c45
youyou <705592307@qq.com>
2023-04-11 23:20:14 +0800

    add session manager

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,4 +31,15 @@ return {
             show_current_context = false,
         },
     },
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+        -- stylua: ignore
+        keys = {
+            { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+        },
+    },
 }
